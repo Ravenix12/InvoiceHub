5e98f818ed1e449a88d320876616bebbf7286ca4
ChewonHan <about0322@naver.com>
2023-07-03 17:02:42 +0800

    Fixed error for deleting and added edit button for each invoice

--- HTML/JS/InvoicePage.js
+++ HTML/JS/InvoicePage.js
@@ -3,11 +3,12 @@
 // data from InvoiceData.js
 import { data } from './InvoiceData.js';
 
+var table = document.getElementById("invoice_table");
+
 // code for rendering the data
 function showtable(data_arr) {
 
   // set up the title of each column
-  var table = document.getElementById("invoice_table");
   table.innerHTML = `
     <tr>
       <th>ID</th>
@@ -21,6 +22,12 @@ function showtable(data_arr) {
 
   // get the icons for delete and more
   for (var i = 0; i < data_arr.length; i++) {
+    var editIcon = `
+    <a href="#">
+      <svg class="ic_edit"xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+      </svg>
+    </a>`;
     var deleteIcon = `
     <svg class="ic_delete" data-id="${data_arr[i].id}" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
@@ -53,32 +60,37 @@ function showtable(data_arr) {
         <td>${data_arr[i].date}</td>
         <td>${data_arr[i].amount}</td>
         <td style="background-color: ${statusColor};">${data_arr[i].status}</td>
-        <td>${deleteIcon} ${moreIcon}</td>
+        <td>${editIcon} ${deleteIcon} ${moreIcon}</td>
       </tr>`;
   }
-
-  // Add event listener for delete icon using event delegation
-  table.addEventListener("click", function (event) {
-    if (event.target.classList.contains("ic_delete")) {
-      // Retrieve the id of the row from the custom attribute
-      var rowId = event.target.getAttribute("data-id");
-  
-      // Find the index of the row with the matching id in the data_arr array
-      var rowIndex = data_arr.findIndex(function (row) {
-        return row.id === rowId;
-      });
-  
-      // Remove the corresponding row from the data_arr array
-      if (rowIndex > -1) {
-        data_arr.splice(rowIndex, 1);
-      }
-  
-      // Re-render the table with updated data
-      showtable(data_arr);
-    }
-  });
  
 }
+
+// Add event listener for delete icon using event delegation
+table.addEventListener("click", function (event) {
+  if (event.target.classList.contains("ic_delete")) {
+    // Retrieve the id of the row from the custom attribute
+    var rowId = event.target.getAttribute("data-id");
+
+    // Find the index of the row with the matching id in the data array
+    var rowIndex = data.findIndex(function (row) {
+      return row.id === rowId;
+    });
+
+    // Show a confirmation popup
+    var confirmed = confirm("Are you sure you want to delete this row?");
+
+    if (confirmed) {
+      // Remove the corresponding row from the data array
+      if (rowIndex > -1) {
+        data.splice(rowIndex, 1);
+      }
+
+      // Re-render the table with updated data
+      showtable(data);
+    }
+  }
+});
 
 
 // execute the code for rendering the data
@@ -172,7 +184,7 @@ function sort(data){
 
 /* SORTING WITH POPUP */
 
-// var button for sort/ soft button
+// // var button for sort button
 // var triggers = document.querySelectorAll('.sort_hint');
 
 // // Add click event listener for the button
